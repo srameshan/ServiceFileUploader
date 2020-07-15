9a0ad953e3e5dd856dc0c9fc8fa7a7a9c957ca12
srameshan <43892429+srameshan@users.noreply.github.com>
2020-07-15 19:26:29 -0400

    First Commit

---
+++ app.js
@@ -0,0 +1,68 @@
+/*
+ Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+*/
+
+
+/*
+ * Import required packages.
+ * Packages should be installed with "npm install".
+ */
+const express = require('express');
+const aws = require('aws-sdk');
+require('dotenv').config();
+/*
+ * Set-up and run the Express app.
+ */
+const app = express();
+app.set('views', './views');
+app.use(express.static('./public'));
+app.engine('html', require('ejs').renderFile);
+app.listen(process.env.PORT || 3000);
+
+/*
+ * Configure the AWS region of the target bucket.
+ * Remember to change this to the relevant region.
+ */
+aws.config.region = 'us-east-1';
+
+/*
+ * Load the S3 information from the environment variables.
+ */
+const S3_BUCKET = process.env.S3_BUCKET_NAME;
+
+/*
+ * Respond to GET requests to /account.
+ * Upon request, render the 'account.html' web page in views/ directory.
+ */
+app.get('/account', (req, res) => res.render('account.html'));
+
+
+app.get('/sign-s3', (req, res) => {
+  const s3 = new aws.S3();
+  const fileName = req.query['file-name'];
+  const fileType = req.query['file-type'];
+  const s3Params = {
+    Bucket: S3_BUCKET,
+    Key: fileName,
+    Expires: 60,
+    ContentType: fileType,
+    ACL: 'public-read'
+  };
+
+  s3.getSignedUrl('putObject', s3Params, (err, data) => {
+    if(err){
+      console.log(err);
+      return res.end();
+    }
+    const returnData = {
+      signedRequest: data,
+      url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+    };
+    res.write(JSON.stringify(returnData));
+    res.end();
+  });
+});
